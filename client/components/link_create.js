30d4482aa2613df3b41970bcb0edc10ebd104f0b
docent <docent@docent.tech>
2017-02-26 19:36:20 -0800

    added submit handlers, prevent default form submit behavior, console logging input values

--- client/components/link_create.js
+++ client/components/link_create.js
@@ -3,12 +3,19 @@ import React, { Component } from 'react';
 class LinkCreate extends Component {
 
   //class based components must use render method, which must return jsx
-render(){
+
+  handleSubmit(event){
+event.preventDefault();
+    console.log(this.refs.input.value);
+  }
+
+  render(){
   return(
-    <form>
+    /*if 'this' isn't bound, it refers to the global object */
+    <form onSubmit={this.handleSubmit.bind(this)}>
     <div className="form-group">
     <label> Link to shorten </label>
-    <input className="form-control" />
+    <input ref="input" className="form-control" />
     </div>
     <button className="btn btn-primary">Shorten!</button>
     </form>
